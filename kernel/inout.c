ce7778c6f48806b655fa81932f4952017e061f8d
diegodayan <39ab1dbb984021b5b60c89011fd4123bb520de51@server>
2012-09-19 18:57:44 -0700

    Adding inportw() function to inout.c

--- kernel/inout.c
+++ kernel/inout.c
@@ -14,3 +14,12 @@ void outportb (unsigned short port, unsigned char value)
 {
     asm ("outb %b0,%w1" : : "a" (value), "Nd" (port));
 }
+
+
+unsigned short inportw (unsigned short port)
+{
+    unsigned short _v;
+
+    asm ("inw %w1,%w0" : "=a" (_v) : "Nd" (port));
+    return _v;
+}
