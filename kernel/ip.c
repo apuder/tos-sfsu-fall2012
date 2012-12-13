a1c9f15eb913ca6a0fb94748fa0369b3afbb4a88
Mehari weldetsion <meharih@mail.sfsu.edu>
2012-12-12 21:38:12 -0800

    further code cleaning .

--- kernel/ip.c
+++ kernel/ip.c
@@ -17,8 +17,8 @@ BOOL is_ip_packet(void *buffer, u_int_t len, IP *ip_pkt)
             u_char_t *ipheader = (u_char_t *)(buf + ETH_HEAD_LEN);
             if(((*(ipheader) & 0x0F)*4) < IP_HEAD_MIN_LEN )
                             return FALSE;
-            ip_pkt->version = ((*(ipheader) >> 4 ) & 0x0F); //first four bits of the first byte
-            ip_pkt->hdr_len  = (*(ipheader) & 0x0F); // second four bits of the first byte
+            ip_pkt->version = ((*(ipheader) >> 4 ) & 0x0F); /* first four bits of the first byte */
+            ip_pkt->hdr_len  = (*(ipheader) & 0x0F); /* second four bits of the first byte */
             ip_pkt->tos = *(ipheader + 1) ;
             ip_pkt->len = *((u_int16_t *)(ipheader + 2));
             ip_pkt->id = *((u_int16_t *)(ipheader + 4));
@@ -28,16 +28,22 @@ BOOL is_ip_packet(void *buffer, u_int_t len, IP *ip_pkt)
             ip_pkt->checksum = *((u_int16_t *)(ipheader + 10));
             memcpy_tos(ip_pkt->src,(u_char_t *)(ipheader + 12),IP_LEN);
             memcpy_tos(ip_pkt->dst,(u_char_t *)(ipheader + 16),IP_LEN);
-            if(ip_pkt->checksum != ip_checksum(ip_pkt))  //compute and check the header checksum
+            if(ip_pkt->checksum != ip_checksum(ip_pkt))  /* compute and check the header checksum */
                             return FALSE;
             return TRUE;
     }
+
+/*
+* This function is used to compute the ip header checksum
+*@param a pointer to an ip structure
+*@returns 16 bit 1's complement of the ip header
+*/
 
 u_int16_t ip_checksum(IP *ip)
 {
 	u_int16_t temp = ip->checksum;
 	ip->checksum = 0;
-	int len = ip->hdr_len <<2; //multiply by 4
+	int len = ip->hdr_len <<2; /* multiply by 4 */
 	unsigned long sum = 0;  /* assume 32 bit long, 16 bit short */
 	const u_int16_t *ip1 = (u_int16_t *)ip;
 
@@ -95,12 +101,12 @@ int inet_aton_tos(u_char_t *dot_ip, u_char_t *net_ip)
 	u_char_t c = *dot_ip;
 	int dot_count = 1;
 
-  for (;;)  //loop through the whole address
+  for (;;)  /* loop through the whole address */
   {
 	if (!is_digit(c))
 		  return -1;
 	val = 0; 
-	for (;;)  // loop till getting a '.'
+	for (;;)  /* loop till getting a '.' */
 	{
 		if (is_digit(c)) 
 		  {
@@ -144,16 +150,16 @@ int create_ip_hr(u_char_t *src_ip,u_char_t *dst_ip,u_int_t payload_len,IP *packe
 
 	packet->version = IP_V4;
 	packet->hdr_len = sizeof(IP) / sizeof(int);
-	packet->tos = IP_TOS_MIN_DELAY; 
+	packet->tos = IP_TOS_MIN_DELAY;            /* minimum service delay */
 	packet->len = htons_tos(packet_len);
-	packet->id =  htons_tos(0xFEED);
-	packet->offset = htons_tos(IP_FLAG_DF);
+	packet->id =  htons_tos(0xFEED);         /* dummy packet id */
+	packet->offset = htons_tos(IP_FLAG_DF); /* don't defragment flag */
 	packet->ttl = IP_DEFAULT_TTL;
 	packet->protocol = IP_PROTO_UDP;
 	packet->checksum = 0;
 	memcpy_tos(packet->src, src_ip, IP_LEN);
 	memcpy_tos(packet->dst, dst_ip, IP_LEN);
-	packet->checksum = ip_checksum(packet); //checksum
+	packet->checksum = ip_checksum(packet); /* checksum */
 
 	return (int)packet_len;
 }
